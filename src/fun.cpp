d3d9575f9f4af484f57d1f79da517c5782eb1318
Rogladon <bakharevskiya@mail.ru>
2021-03-11 22:52:37 +0300

    fix: region Utilits перед рабочими функциями

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,3 +1,52 @@
+
+#pragma region Utilits
+/// <summary>
+/// Checking a character for a digit (from 0 to 9)
+/// </summary>
+/// <param name="ch">checking character</param>
+/// <returns>true - digit, false - other</returns>
+bool isDigit(const char ch) {
+    if (ch >= '0' && ch <= '9') {
+        return true;
+    }
+    return false;
+}
+/// <summary>
+/// Checking a character for a lowecase
+/// </summary>
+/// <param name="ch">checking character</param>
+/// <returns>true - lowercase, false - other</returns>
+bool isLowerChar(const char ch) {
+    if (ch >= 'a' && ch <= 'z') {
+        return true;
+    }
+    return false;
+}
+/// <summary>
+/// Checking a character for Uppercase
+/// </summary>
+/// <param name="ch">checking character</param>
+/// <returns>true - Uppercase, false - other</returns>
+bool isUpperChar(const char ch) {
+    if (ch >= 'A' && ch <= 'Z') {
+        return true;
+    }
+    return false;
+}
+/// <summary>
+/// Rounding to the nearest whole number
+/// </summary>
+/// <param name="d">double number</param>
+/// <returns>int number</returns>
+int round(double d) {
+    int r = int(d);
+    if (d - r < 0.5) return r;
+    return r + 1;
+}
+#pragma endregion
+
+
+
 unsigned int faStr1(const char *str) {
     int count = 0;
     bool inWord = false;
@@ -55,50 +104,3 @@ unsigned int faStr3(const char *str) {
     if (totalLenght == 0 || count == 0) return 0;
     return round(totalLenght / count);
 }
-
-#pragma region Utilits
-/// <summary>
-/// Checking a character for a digit (from 0 to 9)
-/// </summary>
-/// <param name="ch">checking character</param>
-/// <returns>true - digit, false - other</returns>
-bool isDigit(const char ch) {
-    if (ch >= '0' && ch <= '9') {
-        return true;
-    }
-    return false;
-}
-/// <summary>
-/// Checking a character for a lowecase
-/// </summary>
-/// <param name="ch">checking character</param>
-/// <returns>true - lowercase, false - other</returns>
-bool isLowerChar(const char ch) {
-    if (ch >= 'a' && ch <= 'z') {
-        return true;
-    }
-    return false;
-}
-/// <summary>
-/// Checking a character for Uppercase
-/// </summary>
-/// <param name="ch">checking character</param>
-/// <returns>true - Uppercase, false - other</returns>
-bool isUpperChar(const char ch) {
-    if (ch >= 'A' && ch <= 'Z') {
-        return true;
-    }
-    return false;
-}
-/// <summary>
-/// Rounding to the nearest whole number
-/// </summary>
-/// <param name="d">double number</param>
-/// <returns>int number</returns>
-int round(double d) {
-    int r = int(d);
-    if (d - r < 0.5) return r;
-    return r + 1;
-}
-#pragma endregion
-
